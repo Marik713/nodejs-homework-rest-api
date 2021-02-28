fba0690e1a1d9a9f2d68b9f04b8f8c4c3f5c4243
Marik713 <62308344+Marik713@users.noreply.github.com>
2021-02-28 19:50:09 +0200

    with error

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const contactsRouter = require("./routes/api/contactsRouter");
+const app = express();
 
-const contactsRouter = require('./routes/api/contacts')
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const app = express()
+app.use(logger(formatsLogger));
+app.use(cors("*"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.get("/", function (req, res) {
+  res.send("DataBase of Contacts");
+});
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use("/api", contactsRouter);
 
-app.use('/api/contacts', contactsRouter)
+// app.use('*', (req, res) => {
+//     res.status(404).json({
+//         get_contacts: 'http://localhost:3000/api/contacts',
+//         create_contacts: 'http://localhost:3000/api/contacts',
+//         find_contact: 'http://localhost:3000/api/contacts/:contactId',
+//         delete_contacts: 'http://localhost:3000/api/contacts/:contactId',
+//         update_contacts: 'http://localhost:3000/api/contacts/:contactId',
+//     });
+// });
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: "Use api on routes: /api/contacts",
+    data: "Not found",
+  });
+});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+app.use((err, _, res, __) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
 
-module.exports = app
+module.exports = app;
--- model/index.js
+++ model/index.js
@@ -1,15 +1,100 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fsx = require("fs");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
+console.log(contactsPath);
+const contactList = fsx.readFileSync(contactsPath, "utf-8");
+const contactsItems = JSON.parse(contactList);
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  console.log("List of contacts: ");
+  console.table(contactsItems);
 
-const removeContact = async (contactId) => {}
+  return contactsItems;
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const foundContact = await contactsItems.find((contact) => {
+    if (contact.id === contactId) {
+      console.log(`Get contact by ID=${contactId}:`);
+      console.table(contact);
+      return contact;
+    }
+  });
+  if (!foundContact) {
+    console.log("error");
+  }
 
-const updateContact = async (contactId, body) => {}
+  return foundContact;
+};
+
+const removeContact = async (contactId) => {
+  const newContacts = await contactsItems.filter(
+    (contact) => contact.id !== contactId
+  );
+  if (newContacts.length === contactsItems.length) {
+    console.log("error");
+    return contactsItems;
+  }
+
+  console.log("Contact deleted successfully! New list of contacts: ");
+  console.table(newContacts);
+
+  fsx.writeFile(contactsPath, JSON.stringify(newContacts), (error) => {
+    if (error) {
+      return console.log("error :", error);
+    }
+  });
+
+  return newContacts;
+};
+
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  contacts.push({
+    id: contactsItems.length + 1,
+    name: name,
+    email: email,
+    phone: phone,
+  });
+  console.log("Contacts added successfully! New lists of contacts: ");
+  console.table(contactsItems);
+
+  fsx.writeFile(contactsPath, JSON.stringify(contactsItems), (error) => {
+    if (error) {
+      return console.log(error);
+    }
+  });
+
+  return contactsItems;
+};
+
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  const contact = await contactsItems.find((contact) => {
+    if (contact.id === contactId) {
+      contact.name = name;
+      contact.email = email;
+      contact.phone = phone;
+      console.log(`Contact with ID ${contactId} updated!`);
+      console.table(contactsItems);
+      return contact;
+    }
+  });
+
+  if (contact == null) {
+    console.log("error");
+    return;
+  }
+
+  fsx.writeFile(contactsPath, JSON.stringify(contactsItems), (error) => {
+    if (error) {
+      return console.log(error);
+    }
+  });
+
+  return contactsItems;
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +102,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
