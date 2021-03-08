30a9a1996c518c2e0570c3e43f720eb5d36b4201
Marik713 <62308344+Marik713@users.noreply.github.com>
2021-03-08 23:58:37 +0200

    mongoose-CRUD

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,75 +1,21 @@
-const router = require("express").Router();
-const fs = require("fs").promises;
-const functionContacts = require("../../model/index.js");
+const express = require("express");
+const router = express.Router();
+const {
+  getAllContacts,
+  getContactById,
+  createContact,
+  deleteContact,
+  patchContact,
+} = require("../../controllers/control-contacts");
 
-const contacts = async () =>
-  JSON.parse(
-    await fs.readFileSync("../../model/contacts.json", { encoding: "utf-8" })
-  );
+router.get("/", getAllContacts);
 
-router.get("/", async (req, res, next) => {
-  const contactList = await functionContacts.listContacts();
-  res.status(200).json(contactList);
-});
+router.get("/:contactId", getContactById);
 
-router.get("/:contactId", async (req, res, next) => {
-  const contactId = req.params.contactId;
+router.post("/", createContact);
 
-  const foundContact = await functionContacts.getContactById(Number(contactId));
+router.delete("/:contactId", deleteContact);
 
-  if (!foundContact) {
-    return res.status(404).json({ message: "Not found" });
-  }
-
-  res.status(200).json({ foundContact });
-});
-
-router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-
-  if (!name || !email || !phone) {
-    return res.status(400).json({ message: "missing required name field" });
-  }
-
-  const updateContact = functionContacts.addContact(name, email, phone);
-
-  res.status(201).json(updateContact);
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  const contactId = req.params.contactId;
-
-  const newContacts = await functionContacts.removeContact(Number(contactId));
-
-  if (newContacts.length === contacts.length) {
-    return res.status(400).json({ message: "Not found" });
-  }
-
-  res.status(200).json({
-    message: `Contact with ID=${contactId} deleted successfully!`,
-  });
-});
-
-router.patch("/:contactId", async (req, res, next) => {
-  const contactId = req.params.contactId;
-  const { name, email, phone } = req.body;
-
-  if (!name || !email || !phone) {
-    return res.status(400).json({ message: "missing required name field" });
-  }
-
-  const updatedContacts = functionContacts.updateContact(
-    Number(contactId),
-    name,
-    email,
-    phone
-  );
-
-  if (!updatedContacts) {
-    return res.status(404).json({ message: "Not found" });
-  }
-
-  res.status(200).json(updatedContacts);
-});
+router.patch("/:contactId", patchContact);
 
 module.exports = router;
